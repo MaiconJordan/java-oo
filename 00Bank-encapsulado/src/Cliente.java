e993d7fbb9302ae6d8b3849a85ebf1b351e5f289
Maicon Jordan <maiconplus@gmail.com>
2022-06-26 17:26:00 -0300

    getter e setters

---
+++ 00Bank-encapsulado/src/Cliente.java
@@ -0,0 +1,32 @@
+
+public class Cliente {
+
+	private String nome;
+	private String CPF;
+	private String Profissao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getProfissao() {
+		return Profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		Profissao = profissao;
+	}
+
+}
