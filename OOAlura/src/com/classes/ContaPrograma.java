ea36f1a6171169a7621196b52c3f101cc89ff316
Maicon Jordan <maiconplus@gmail.com>
2022-06-25 22:02:11 -0300

    Classes e metodos

---
+++ OOAlura/src/com/classes/ContaPrograma.java
@@ -0,0 +1,31 @@
+package com.classes;
+
+public class ContaPrograma {
+
+	public static void main(String[] args) {
+		
+		Conta primeiraConta = new Conta();
+		
+		primeiraConta.deposita(1000);
+		
+		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saca(500);
+		
+		System.out.println(primeiraConta.saldo);
+		
+		
+		// Segunda conta
+		
+		Conta segundaConta = new Conta();
+		
+		segundaConta.deposita(2000);
+		
+		segundaConta.transfere(500, primeiraConta);
+		
+		System.out.println(primeiraConta.saldo);
+		System.out.println(segundaConta.saldo);
+		
+	}
+
+}
