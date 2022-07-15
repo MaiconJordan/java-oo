60b9bb63ed9c40369c70686e383ca3698bddbd18
Maicon Jordan <maiconplus@gmail.com>
2022-07-15 19:14:09 -0300

    criando JAR

---
+++ bytebank-biblioteca/src/br/com/alura/bytebank/TesteBibliotecan.java
@@ -0,0 +1,16 @@
+package br.com.alura.bytebank;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBibliotecan {
+
+	public static void main(String[] args) {
+	
+		
+		Conta conta = new ContaCorrente (123, 1223);
+		
+		
+	}
+
+}
