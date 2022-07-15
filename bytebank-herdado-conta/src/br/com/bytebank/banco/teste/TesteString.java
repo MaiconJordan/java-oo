ed99241149ffecbdee77ec8c2be2e3c5f44d6fe7
Maicon Jordan <maiconplus@gmail.com>
2022-07-15 19:41:31 -0300

    String e imutabilidade

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,21 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		String nome = "Alura";
+		
+		
+		String comreplace = nome.replace("A", "C");
+		
+		String minuscula = nome.toLowerCase();
+		
+		
+		System.out.println(comreplace);
+		System.out.println(minuscula);
+		System.out.println(nome);
+		
+		
+	}
+}
